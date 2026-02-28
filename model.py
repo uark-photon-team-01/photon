4de4d156e7cd360df45d6734dedb585dcff15132
Centarrius Brooks <centarib@gmail.com>
2026-02-28 17:58:04 -0600

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,30 @@
 from dataclasses import dataclass, field
-from typing import List #Just makes to code easier to read when declaring new Lists
+from typing import List # Just makes to code easier to read when declaring new Lists
 
-@dataclass #dataclasses allow you write "data - only classes", that way you can declare fields and python writes "init" for you.
+
+@dataclass # dataclasses allow you write "data - only classes", that way you can declare fields and python writes "init" for you.
 class PlayerData:
     playerID: int
     codename: str
     equipmentID: int
     team: str
     score: int = 0
-    has_baseIcon: bool = False #Just Checks to see does this player have a base icon next to thier name
+    has_baseIcon: bool = False
 
 
 @dataclass
-class Game_State: # field(default_factory=list) makes sure that every new instance of GameState gets it's own list
-    redTeam: List[PlayerData] = field(default_factory=list) #Team rosters
+class Game_State:  # field(default_factory=list) makes sure that every new instance of GameState gets it's own list
+    redTeam: List[PlayerData] = field(default_factory=list)  # Team rosters
     greenTeam: List[PlayerData] = field(default_factory=list) # Typical lists like [] are shared across multiple instances in Python
-    phase: str = "Beginning" #What screen/phase the player is in
-    time_remaining: int = 0 #Time Left
+
+    phase: str = "Beginning" # What screen/phase the player is in
+    time_remaining: int = 0 # Time Left
     eventLog: List[str] = field(default_factory=list)
+
+    # timer support
+    timer_running: bool = False
 
 
 if __name__ == "__main__":
     gs = Game_State()
-    print("This model works. Yay! The game is currrently in the ", gs.phase, " phase.")
+    print("This model works. Yay! The game is currently in the", gs.phase, "phase.")
