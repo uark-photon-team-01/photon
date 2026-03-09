4110560bc152c5c3c86a0ff9bea30485983ff041
Centarrius Brooks <centarib@gmail.com>
2026-03-09 14:13:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-set -e 
+set -e
 
-echo "The following above means 'run this file with bash.'"
-echo "Set -e means 'stop if any command fails.'"
+echo "The line above means: run this file with bash."
+echo "set -e means: stop if any command fails."
 
-
-echo "Updating package list."
+echo "Updating package list..."
 sudo apt-get update
 
-echo "Automatically installing python, Tkinter GUI support,"
-echo "PostgreSQL Driver, and the package installer (for extra liberies)."
-sudo apt-get install -y python3 python3-tk python3-psycopg2 python3-pip python3-pil python3-pygame
+echo "Installing required packages..."
+sudo apt-get install -y git python3 python3-tk python3-psycopg2 python3-pip python3-pil python3-pygame
 
 echo "Install complete."
-echo "To run this project, please use the following command: python3 main.py"
+echo "To run the project:"
+echo "  1. sudo systemctl start postgresql"
+echo "  2. python3 main.py"
