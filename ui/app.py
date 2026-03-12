144b2c0f0d067a45440180527cb7285454899b75
Centarrius Brooks <centarib@gmail.com>
2026-03-12 04:16:50 -0500

    Update app.py

--- ui/app.py
+++ ui/app.py
@@ -433,7 +433,6 @@ class ActionScreen(tk.Frame):
         self.trackFiles = []
         self._music_started = False
         self._music_file = None
-        self.startCueFile = None
         self._last_phase = None
 
         self._load_base_icon()
@@ -470,7 +469,6 @@ class ActionScreen(tk.Frame):
     
         assets_dir = os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "assets"))
         tracks_dir = os.path.join(assets_dir, "photon_tracks")
-        self.startCueFile = os.path.join(assets_dir, "game sounds", "Photon Start.wav")
     
         if os.path.isdir(tracks_dir):
             self.trackFiles = sorted(glob.glob(os.path.join(tracks_dir, "*.mp3")))
@@ -497,33 +495,7 @@ class ActionScreen(tk.Frame):
             self._music_started = False
             self._music_file = None
     
-    
-    def _start_warning_music(self):
-        if self._music_started or not self.trackFiles:
-            return
-    
-        
-        self._play_music_file(self.trackFiles[0], loops=0)
-    
-    
-    def _start_live_music(self):
-        if self._music_started or not self.trackFiles:
-            return
-    
-        live_choices = self.trackFiles[1:] if len(self.trackFiles) > 1 else self.trackFiles
-        self._play_music_file(random.choice(live_choices), loops=-1)
-    
-    
-    def _play_begin_cue(self):
-        if not PYGAME_OK or not self.startCueFile or not os.path.exists(self.startCueFile):
-            return
-    
-        try:
-            sound = pygame.mixer.Sound(self.startCueFile)
-            sound.play()
-        except Exception:
-            pass
-    
+
     
     def _stop_music(self):
         if PYGAME_OK:
@@ -540,18 +512,8 @@ class ActionScreen(tk.Frame):
         if phase == self._last_phase:
             return
     
-        if phase == "WARNING":
-            self._stop_music()
-            self._start_warning_music()
-    
-        elif phase == "PLAYING":
-            if self._last_phase == "WARNING":
-                self._stop_music()
-                self._play_begin_cue()
-                self.after(250, self._start_live_music)
-            else:
-                self._start_live_music()
-    
+        if phase in ("WARNING", "PLAYING"):
+            self._start_session_track()
         else:
             self._stop_music()
     
